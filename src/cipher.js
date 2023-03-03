095fe7ac6519b34efe85609a6c901e675d21e421
NaraSakamoto <nara.sakamoto@gmail.com>
2023-03-03 16:51:31 -0300

    bugfix

--- src/cipher.js
+++ src/cipher.js
@@ -5,24 +5,18 @@ const cipher = {
 const commonAlphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 function encode(offset, message){
-  console.log("offset: " + offset)
-  console.log("message: " + message)
   if(!offset || !message) throw new TypeError;
 
   let encryptMessage = "";
 
   for(let i = 0; i < message.length; i++){
     const letter = message[i];
-    console.log("letter: " + message)
     const index = indexOfLetter(letter, commonAlphabet)
-    console.log("index: " + index)
 
     if(index === -1){
       encryptMessage += letter;
     } else {
       let newIndex = (index+offset) % commonAlphabet.length;
-      console.log("newIndex: " + newIndex)
-
       if(newIndex < 0) newIndex = commonAlphabet.length + newIndex;
       encryptMessage += commonAlphabet[newIndex];
     }
