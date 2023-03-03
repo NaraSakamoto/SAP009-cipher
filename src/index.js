eba1a35eb2eade0821e4b98dfa88bd2b4d5a497b
NaraSakamoto <nara.sakamoto@gmail.com>
2023-03-03 12:57:38 -0300

    Adding function decode to button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import cipher from './cipher.js';
 
 document.getElementById("encodeBtn").addEventListener("click", encode);
+document.getElementById("decodeBtn").addEventListener("click", decode);
 
 function encode(){
   const textToEncode = document.getElementById("textToEncode").value;
-  const offset = document.getElementById("encodeOffset").value;
+  const offset = document.getElementById("offset").value;
   
   document.getElementById("textToDecode").value = cipher.encode(offset, textToEncode);
 
+  alert("Texto traduzido com sucesso!")
+}
+
+function decode(){
+  const textToDecode = document.getElementById("textToDecode").value;
+  const offset = document.getElementById("offset").value;
+    
+  document.getElementById("textToEncode").value = cipher.decode(offset, textToDecode);
+  
   alert("Texto traduzido com sucesso!")
 }
